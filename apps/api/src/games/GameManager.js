bef29180d26fb9e6803e0fc1af08123c21762c17
nilsw13 <nils.wenting@laplateforme.io>
2026-03-18 13:59:28 +0100

    add test and minimal code for GameManager

--- apps/api/src/games/GameManager.js
+++ apps/api/src/games/GameManager.js
@@ -2,11 +2,25 @@ import Morpion from "./morpion/game.js";
 
 export default class GameManager {
    constructor() {
+       this.games = new Map();
    }
 
-   launchGame(room, game) {
-       if (game === 'morpion') {
-           return new Morpion(room);
+   launchGame(gameId, type) {
+       let game;
+       if (type === 'morpion') {
+           game =  new Morpion(gameId);
        }
+       this.games.set(gameId, game);
+       return game;
    }
+
+   getGame(gameId) {
+        return this.games.get(gameId);
+   }
+
+   removeGame(gameId) {
+       this.games.delete(gameId);
+   }
+
+
 }
--- apps/api/tests/gameManager.test.js
+++ apps/api/tests/gameManager.test.js
@@ -12,7 +12,25 @@ describe("GameManager test", () => {
 
     test("le gameManage doit recuperer une instance d'un jeu via son room id", () => {
         const manager = new GameManager();
-        const createdGame = manager.launchGame('fun', 'morpion');
-        const retrievedGame = manager.getGame('fun');
+        const createdGame = manager.launchGame(1, 'morpion');
+        const retrievedGame = manager.getGame(1);
+        expect(retrievedGame).toBe(createdGame);
+    })
+
+    test("Le gameManager doit férer plusieurs parties différentes de maniere isolées", () => {
+        const manager = new GameManager();
+        const game1 = manager .launchGame(1, 'morpion');
+        const game2 = manager.launchGame(2, 'morpion');
+        game1.playMove(0);
+        const gameboard1 = game1 .getBoard();
+        const gameboard2 = game2 .getBoard();
+        expect(gameboard1[0]).toEqual('X');
+        expect(gameboard2[0]).toEqual('');
+    })
+
+    test("Doit pouvoir supprimer une partie une fois terminée", () => {
+        const manager = new GameManager();
+        manager.removeGame(1);
+        expect(manager.getGame(1)).toBeUndefined();
     })
 })

3b3d0f925fb9a9bf9840b36e558905381cb1bf37
nilsw13 <nils.wenting@laplateforme.io>
2026-03-20 12:37:43 +0100

    add query client

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -3,11 +3,22 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './assets/style/main.scss'
 import App from './App.jsx'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus : false,
+            staleTime: Infinity
+        }
+    }
+})
 createRoot(document.getElementById('root')).render(
     <StrictMode>
+        <QueryClientProvider client={queryClient}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+        </QueryClientProvider>
     </StrictMode>,
 )

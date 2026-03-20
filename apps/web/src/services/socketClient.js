b2c0680cccb5bb9582f4e37515ec09c097327752
nilsw13 <nils.wenting@laplateforme.io>
2026-03-20 12:35:05 +0100

    add game handlers

--- apps/web/src/services/socketClient.js
+++ apps/web/src/services/socketClient.js
@@ -13,8 +13,14 @@ export const disconnectSocket = () => {
 
 export const joinRoom = (pseudo, age, roomChoice) => {
     if (socket) {
-        socket.connect();
-        socket.emit("join_room", {pseudo, age, roomChoice});
+        if (socket.connected) {
+            socket.emit("join_room", {pseudo, age, roomChoice});
+        } else {
+            socket.connect();
+            socket.once('connect', () => {
+                socket.emit("join_room", {pseudo, age, roomChoice});
+            });
+        }
     }
 };
 
@@ -49,5 +55,55 @@ export const leaveRoom = (room)=> {
 export const onRoomInfo = (callback) => {
     if (socket) socket.on('room_info', callback);
 };
+
+
+// ======= GAME HANDLERS =======
+export const startMatch = (opponentId) => {
+    if (socket) socket.emit('game:start', {opponentSocketId : opponentId});
+}
+
+export const playMove = (matchId, index) => {
+    if (socket) socket.emit('game:playMove', {matchId: matchId, index: index});
+}
+
+export const onGameReady = (callback) => {
+    if (socket) socket.on('game:ready', callback);
+}
+
+export const onGameUpdate = (callback) => {
+    if (socket) socket.on('game:updated', callback);
+}
+
+export const offGameEvents = () => {
+    if (socket) {
+        socket.off('game:ready');
+        socket.off('game:updated');
+    }
+};
+
+// ========== CHALLENGE HANDLERS
+export const sendChallenge = (targetSocketId, game) => {
+    if (socket) socket.emit('game:challenge_user', {targetSocketId, game});
+};
+
+export const respondToChallenge = (senderSocketId, accept, game ) => {
+    if (socket) socket.emit('game:respond_challenge', {senderSocketId, accept, game});
+};
+
+export const onReceiveChallenge = (callback) => {
+    console.log("socket au moment de onReceiveChallenge:", socket?.id, socket?.connected);
+    if (socket) socket.on('game:incoming_challenge', (data) => {
+        console.log("incoming_challenge reçu:", data);
+        callback(data);
+    });
+};
+
+export const offChallengeEvents = () => {
+    if (socket) {
+        socket.off('game:incoming_challenge');
+    }
+}
+
+export const getSocketId = () => socket?.id;
 
 export const getSocket = () => socket;

74a806a28e4804568ab26936c296c3640aed514d
nilsw13 <nils.wenting@laplateforme.io>
2026-03-20 12:40:00 +0100

    refacto rules

--- apps/api/src/games/morpion/rules.js
+++ apps/api/src/games/morpion/rules.js
@@ -1,10 +1,3 @@
-// DEFINE WINNING RULES
-export const winningCombinaisons = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontales
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Verticales
-    [0, 4, 8], [2, 4, 6]              // Diagonales
-];
-
 export const states = {
     PLAYING: 0,
     WIN: 1,
